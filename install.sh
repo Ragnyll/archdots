f5625bc96d23032739d64e8c20f909c07b3b75c7
Ragnyll <ragnyll@protonmail.com>
2019-09-11 01:38:50 +0000

    making progress

--- install.sh
+++ install.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-sudo pacman -Syu wget lightdm lightdm-gtk-greeter bspwm sxhkd rxvt-unicode dmenu firefox unzip feh
+# Insall pacman dependencies dependencies
+sudo pacman -Syu wget lightdm lightdm-gtk-greeter bspwm sxhkd rxvt-unicode dmenu firefox unzip feh openssh zsh thunar pulseaudio compton
+
+#Install yay
+git clone https://aur.archlinux.org/yay.get
+cd yay
+makepkg -si
+cd .. && rm -rf yay/
+
+# Install yay dependencies (Lots of manual intervention here. Be careful)
+# First make sure yay is up to date
+yay
+yay -S polybar
+
+# symlink nessecary confs
+# DESTRUCTIVE
+ln -s ~/.dotfiles/bin ~/bin
 mkdir -p ~/.config/bspwm/
 ln -sf ~/.dotfiles/config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
 mkdir -p ~/.config/sxhkd/
 ln -sf ~/.dotfiles/config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+mkdir -p ~/.config/polybar
+ln -sf ~/.dotfiles/config/polybar/config ~/.config/polybar/config
+ln -sf ~/.dotfiles/config/polybar/config ~/.config/polybar/launch.sh
