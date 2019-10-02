b7d0b16a33cc4bd27f983d1abad9cc687f484613
Ragnyll <ragnyll@protonmail.com>
2019-10-01 23:38:28 -0500

    added time/date indicator for i3lock

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # script modified from https://www.reddit.com/r/unixporn/comments/7df2wz/i3lock_minimal_lockscreen_pretty_indicator/
 
-i3lock -n -i /home/ragnyll/Pictures/wallpapers/anime/lockscreen.png \
+i3lock -n -i /home/ragnyll/Pictures/wallpapers/anime/lockscreen.png --tiling \
     --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
     --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
     --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
     --ringvercolor=ffffffff --ringwrongcolor=ffffffff --indpos="x+86:y+1003" \
-    --radius=15 --veriftext="" --wrongtext="" --tiling
+    --radius=15 --veriftext="" --wrongtext="" \
+	--force-clock --timecolor=ffffffff --datecolor=ffffffff --timepos="x+186:y+1003" \
+	--noinputtext=""
