ee019f43841238b0f8f2d8f3f59b3ba19e80b912
ragnyll <ragnyll@protonmail.com>
2020-01-12 17:00:14 -0600

    added draw_rule to end of screen

--- conky_scripts/cpu_gauge.lua
+++ conky_scripts/cpu_gauge.lua
@@ -11,10 +11,11 @@ function conky_main()
                                          conky_window.height)
     cr = cairo_create(cs)
 
-	gauge_props = {
-		gauge_name = "cpu",
-		gauge_value = conky_parse("${cpu}"),
-		gauge_max = 100,
+	gaugue_props = {
+		gaugue_name = "cpu",
+		gaugue_value = conky_parse("${cpu}"),
+		gaugue_max = 100,
+		orientation = "right",
 		colors = {
 			free_color = { -- dark_grey
 				r = 35.9,
@@ -26,7 +27,13 @@ function conky_main()
 				r = 52.9,
 				g = 51.3,
 				b = 54.6,
-				a = .4
+				a = .6
+			},
+			rule_color = { -- light_grey less transparency
+				r = 52.9,
+				g = 51.3,
+				b = 54.6,
+				a = .5
 			}
 		},
 		meter = {
@@ -34,30 +41,59 @@ function conky_main()
 			y = 200, -- the center y offset relative to conf bottom left
 			r = 50,  -- the radius of the guage
 			w = 4    -- the width of the stroke
+		},
+		rule = {
+			w = 3,
+			l = 285
 		}
 	}
 
 	-- draw the cpu_indicator
-	cpu_indicator_gauge(cr, gauge_props)
+	ring_gaugue(cr, gaugue_props)
 
     cairo_destroy(cr)
     cairo_surface_destroy(cs)
     cr = nil
 end
 
-function cpu_indicator_gauge(cr, gauge_props)
-	-- draws a ring indicator for cpu usage
+function ring_gaugue(cr, gaugue_props)
+	-- draws a ring gaugue
 	-- cr: cairo created surface
-	-- gauge_props: table containing colors (free and used) and meter properties
+	-- gaugue_props: table containing colors (free and used), a meter table, and gaugue metadata
+	-- draw_rule: boolean to tell whether to draw a rule connecting the ring to the oriented edge of the cairo space
 
-	draw_free_ring(cr, gauge_props)
-	draw_used_ring(cr, gauge_props)
+	if(gaugue_props.rule ~= nil) then
+		draw_rule(cr, gaugue_props)
+	end
+	draw_free_ring(cr, gaugue_props)
+	draw_used_ring(cr, gaugue_props)
+end
+
+function draw_rule(cr, gaugue_props)
+	-- draws a rule from the top middle of the circle to the edge of the cairo space the gaugue is oriented to
+	-- gaugue_props: the gaugue table containing the rule table
+
+	-- find top center of ring and connect the rule to it at a 45 degree angle (its not really a 45 degree angle. I dont feel like calculating that atm)
+	tangent_x = gaugue_props.meter.x
+	tangent_y = gaugue_props.meter.y - gaugue_props.meter.r - (gaugue_props.meter.w / 2)
+	end_x, end_y = tangent_x + 25, tangent_y - 25
+	cairo_set_source_rgba(cr, gaugue_props.colors.rule_color.r, gaugue_props.colors.rule_color.g, gaugue_props.colors.rule_color.b, gaugue_props.colors.rule_color.a)
+	cairo_set_line_width(cr, gaugue_props.rule.w)
+	cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND)
+	cairo_move_to(cr, tangent_x, tangent_y)
+	cairo_line_to(cr, end_x, end_y)
+	cairo_stroke(cr)
+
+	-- find the end coordinate of the connector rule and draw another rule connecting it to the edge of the screen (note this value is estimated as there is no way (i know of) to get the size of the cairo space
+	cairo_move_to(cr, end_x, end_y)
+	cairo_line_to(cr, end_x + gaugue_props.rule.l, end_y)
+	cairo_stroke(cr)
 end
 
 function draw_free_ring(cr, ring_props)
 	-- draws a dark gray ring
 	-- cr: cairo created surface
-	-- ring_props: table containing colors (free and used) and meter properties
+	-- ring_props: table containing colors (free and used), a meter table, and gaugue metadata
 
 	start_angle, end_angle = 0, (2 * math.pi)
 	cairo_set_source_rgba(cr, ring_props.colors.free_color.r, ring_props.colors.free_color.g, ring_props.colors.free_color.b, ring_props.colors.free_color.a)
@@ -69,12 +105,11 @@ end
 function draw_used_ring(cr, ring_props)
 	-- draws a light circle indicating amount of resource used
 	-- cr: cairo created surface
-	-- ring_props: table containing colors (free and used) and meter properties
+	-- ring_props: table containing colors (free and used), a meter table, and gaugue metadata
 
 	-- move arc_indicator
-	end_angle = ring_props.gauge_value * (360 / ring_props.gauge_max)
+	end_angle = ring_props.gaugue_value * (360 / ring_props.gaugue_max)
 
-	-- Draw bg
 	cairo_set_source_rgba(cr, ring_props.colors.used_color.r, ring_props.colors.used_color.g, ring_props.colors.used_color.b, ring_props.colors.used_color.a)
 	cairo_set_line_width(cr, ring_props.meter.w)
 	-- angles in cairo are drawn from pi (90 deg) they must be offset to start from top
