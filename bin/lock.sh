4b9f61398fdc5b580e07148ee92d007d551d211b
ragnyll <ragnyll@protonmail.com>
2020-01-12 22:36:33 -0600

    updated lock image

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # script modified from https://www.reddit.com/r/unixporn/comments/7df2wz/i3lock_minimal_lockscreen_pretty_indicator/
 
-i3lock -n -i /home/ragnyll/Pictures/wallpapers/anime/broken_tokyo.png --tiling \
+i3lock -n -i /home/ragnyll/Pictures/wallpapers/anime/hacker_girl.jpg --tiling \
     --insidecolor=373445ff --ringcolor=ffffffff --line-uses-inside \
     --keyhlcolor=d23c3dff --bshlcolor=d23c3dff --separatorcolor=00000000 \
     --insidevercolor=fecf4dff --insidewrongcolor=d23c3dff \
