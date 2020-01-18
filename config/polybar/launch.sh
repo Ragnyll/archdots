32cbd34bde073b5ad559e740159a89b69490c622
ragnyll <ragnyll@protonmail.com>
2020-01-18 15:15:28 -0600

    updated polybar with font-awesome icons

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,9 +3,9 @@ killall polybar
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload ragbar &
   done
 else
-  polybar --reload example &
+  polybar --reload ragbar &
 fi
 
