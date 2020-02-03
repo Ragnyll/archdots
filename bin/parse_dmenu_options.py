0edb47a1ce7ba01cf0e1cccf6033bca60399aa48
ragnyll <ragnyll@protonmail.com>
2020-02-02 21:06:31 -0600

    My dmneu script mode actually works now

--- bin/parse_dmenu_options.py
+++ bin/parse_dmenu_options.py
@@ -12,8 +12,7 @@ def build_cmd_with_option(file_name):
         cmd_and_options = [line.rstrip() for line in file.readlines() if string_to_search in line]
     # there should only ever be one match based on how dmenu will return file names
     # this will break on commands where there are no options
-    cmd = cmd_and_options[0].split(',')[0]
-    print(cmd)
+    cmd = cmd_and_options[0].split(',')[1]
     options = cmd_and_options[0].split(',')[1:]
     for option in options:
         print(cmd + ' ' + option)
