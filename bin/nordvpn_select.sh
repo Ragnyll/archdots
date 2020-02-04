d8a047f41d9e67d07f10069bd4bd000c766dbc29
ragnyll <ragnyll@protonmail.com>
2020-02-03 23:32:47 -0600

    added option for fastet vpn

--- bin/nordvpn_select.sh
+++ bin/nordvpn_select.sh
@@ -5,6 +5,8 @@ if [ $(ps aux | grep nordvpnd | wc -l) -gt 1 ]; then
 	sudo systemctl stop nordvpnd.service
 else
 	sudo systemctl start nordvpnd.service
+	if [ $(echo $1 | grep default | wc -l) -eq 1 ]; then
+		nordvpn c
 	if [ $(echo $1 | grep Chicago | wc -l) -eq 1 ]; then
 		nordvpn c Chicago
 	elif [ $(echo $1 | grep Toronto | wc -l) -eq 1 ]; then
