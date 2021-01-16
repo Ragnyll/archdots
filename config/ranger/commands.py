585166cbfcb4ed0c17fa72f86c2644fd69bfdab4
ragnyll <ragnyll@protonmail.com>
2021-01-15 22:55:26 -0600

    added a dmenu script for ffmpeg

--- config/ranger/commands.py
+++ config/ranger/commands.py
@@ -6,11 +6,6 @@ import os
 from os import getxattr, setxattr, listxattr, removexattr
 
 from ranger.api.commands import Command
-from gnupg import GPG
-
-import tarfile
-from time import sleep
-import subprocess
 
 
 def append_xattr_to_file(fname, xattr_val, xattr_key='user.tags'):
@@ -66,97 +61,6 @@ def remove_xattr_value_from_file(fname, xattr_val, xattr_key='user.tags'):
                 pass
 
         return
-
-
-class encrypt(Command):
-    """:encrypt
-
-    Encrypts a file or dir (as a tar.gz) with the default gpg key
-    """
-
-    def tardir(self, path):
-        """:tardir
-
-        tars a directory into a dir of the same name appended with .tar.gz
-
-        returns the name of the tarfile
-        """
-        output_path = path + '.tar.gz'
-
-        with tarfile.open(output_path, "w:gz") as tar_handle:
-            for root, dirs, files in os.walk(path):
-                for file in files:
-                    tar_handle.add(os.path.join(root, file))
-
-        return output_path
-
-    def get_default_pub_key(self, gpg_home):
-        """
-
-        returns False if the default_key was not found
-        """
-        with open(os.path.join(gpg_home, 'gpg.conf'), 'r') as gpg_conf:
-            line = [line for line in gpg_conf.readlines() if 'default-key' in line]
-            if not line:
-                return False
-
-        return line[0].split(' ')[1][:-1]
-
-    def execute(self):
-        gpg_home = os.path.join(os.path.expanduser('~'), '.gnupg/')
-        default_key = self.get_default_pub_key(gpg_home)
-
-        if not default_key:
-            self.fm.notify('default-key configuration could not be found in ~/.gnupg/gpg.conf')
-            return
-
-        gpg = GPG(gpgbinary='/usr/bin/gpg', gnupghome=gpg_home)
-
-        paths = [os.path.basename(f.path) for f in self.fm.thistab.get_selection()]
-
-        for p in paths:
-            if os.path.isdir(p):
-                new_p = self.tardir(p)
-                subprocess.run(['rm', '-rf', p])
-                p = new_p
-
-            with open(p, 'rb') as f:
-                enc = gpg.encrypt_file(f, default_key)
-
-            with open(p + '.gpg', 'wb+') as out:
-                out.write(enc.data)
-
-            if os.path.isfile(p):
-                os.remove(p)
-
-
-class decrypt(Command):
-    """:decrypts
-
-    Decrypts a file with gpg or a directory by extracting a tar file and decrypting it
-
-    passing true as the false flag will not delete the origin gpg file
-    """
-
-    def execute(self):
-        gpg = GPG(gnupghome=os.path.join(os.path.expanduser('~'), '.gnupg'))
-
-        paths = [os.path.basename(f.path) for f in self.fm.thistab.get_selection()]
-
-        for p in [p for p in paths if p.endswith('gpg')]:
-            with open(p, 'rb') as enc:
-                dec_b = gpg.decrypt_file(enc)
-
-            out_fname = os.path.splitext(p)[0]
-            with open(out_fname, 'wb+') as dec_f:
-                dec_f.write(dec_b.data)
-
-            if self.arg(1) != 'true':
-                os.remove(p)
-
-            if tarfile.is_tarfile(out_fname):
-                tarfile.open(out_fname).extractall(path='.')
-                os.remove(out_fname)
 
 
 class add_tag(Command):
