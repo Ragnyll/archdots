eb3a4b0b215a8279cd85ad20b14066b7874d4309
ragnyll <ragnyll@gallowzhumour.dev>
2022-04-19 23:43:45 -0500

    alacrity config and install update

--- arch_install.sh
+++ arch_install.sh
@@ -40,6 +40,7 @@ ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/config ~/.config
 ln -sf ~/.dotfiles/surf ~/.surf
 ln -sf ~/.dotfiles/config/prompts/ ~/.config
+ln -sf ~/.dotfiles/config/alacritty/ ~/.config
 
 # install mutt
 git clone git@gitlab.com:Ragnyll/my_mutt.git && cd my_mutt && install.sh && cd ..
