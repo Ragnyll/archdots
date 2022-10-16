f9cc71c86f56c2d5a19ab38b1c6d5acd475885f9
ragnyll <ragnyll@gallowzhumour.dev>
2022-10-15 20:48:20 -0700

    some updates

--- post_upgrade.sh
+++ post_upgrade.sh
@@ -10,8 +10,6 @@ echo "rebuilding python env"
 
 # re build other installs
 paru -R nvidia-settings && paru -S nvidia-settings
-paru -R ranger-git && paru -S ranger-git
-paru -R optimus-manager && paru -S optimus-manager
 paru -R npm node-gyp nodejs semver && paru -S npm node-gyp nodejs semver
 
 printf '========== Post Upgrade Step Complete! ==========
