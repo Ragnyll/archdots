3ba096398c3c97502c27b5bb6a803192e6843258
Ragnyll <justanotherjakeyg@gmail.com>
2025-05-30 22:34:06 -0700

    removing old bin files

--- bin/conky_toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# toggles conky being on or off
-
-if [ $(ps aux | grep conky | wc -l) -ge 4 ]; then
-	pkill -f conky
-else
-	conky -d
-fi
-
--- bin/polybar_toggle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $(ps aux | grep "polybar --reload ragbar" | wc -l) -ge 2 ]; then
-	pkill -f "polybar --reload ragbar"
-else
-    ~/.config/polybar/launch.sh &
-fi
